9295c7092a44086d9a8203f6c1c04f934b311ee1
Mokey2002 <edventura91@gmail.com>
2019-11-26 14:51:14 -0800

    Added api to retrieve images from aws

--- back-end/routes/upload.js
+++ back-end/routes/upload.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const classify = require('../utils/index').classify
 const validateToken = require('../utils/index').validateToken
+var AWS = require('aws-sdk');
 
 router.post('/', multerConfig.upload, (req, res) => {
   classify(req.file.buffer).then(response => {
@@ -15,5 +16,32 @@ router.post('/', multerConfig.upload, (req, res) => {
 router.post('/save', validateToken, multerConfig.uploadToS3, (req, res) => {
   res.status(200).send('Successfully uploaded file')
 })
+
+app.get('/image/:keyimg',(req,res) =>{
+  AWS.config.update(
+  {
+    accessKeyId: "",
+    secretAccessKey: "",
+  }
+);
+   var s3 = new AWS.S3();
+   var params=  { Bucket: "", Key: req.params.keyimg };
+   const path =".."
+   var tempFileName = req.params.keyimg;
+   var tempFile = fs.createWriteStream(tempFileName);
+   s3.getObject(params).createReadStream().on('error', error => {
+   fs.unlink(req.params.keyimg, function (err) {
+    if (err) throw err;
+    // if no error, file has been deleted successfully
+    console.log('File deleted!');
+}); 
+	return res.status(400).json('error');
+
+}).pipe(tempFile).on('finish',() => {
+  // stream has ended
+	return res.status(200).json('sucess');
+});
+
+});
 
 module.exports = router
