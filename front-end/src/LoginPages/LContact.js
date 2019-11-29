fe2ae86712bb75acf1f6fc03a8587c8c7343f938
raz4 <muslim.razi@seeloz.com>
2019-11-29 15:44:49 -0800

    Update LContact.js

--- front-end/src/LoginPages/LContact.js
+++ front-end/src/LoginPages/LContact.js
@@ -3,11 +3,11 @@ export const LContact = () => (
     <div>
        <h2>Ryan Choy</h2>
        <h3>zheyuanryan.choy@sjsu.edu</h3>
-       <h2>name</h2>
-       <h3>email</h3>
-       <h2>name</h2>
-       <h3>email</h3>
-       <h2>name</h2> 
-       <h3>email</h3>
+       <h2>Muslim Razi</h2>
+       <h3>muslim.razi@sjsu.edu</h3>
+       <h2>Eduardo Lemus</h2>
+       <h3>edu.lemuss@gmail.com</h3>
+       <h2>Robert Campbell</h2> 
+       <h3>robertgcampbell95@gmail.com</h3>
     </div>
 )
