1a2f1fd7491baacc2d1345b642f1b077c03d7cff
Robert <robertgcampbell95@gmail.com>
2019-12-07 23:14:40 -0800

    Remove about and contact from navbar

--- front-end/src/components/NavigationBar.js
+++ front-end/src/components/NavigationBar.js
@@ -22,8 +22,6 @@ export const NavigationBar = () =>(
             <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
             </Navbar.Collapse>
         </Navbar>
